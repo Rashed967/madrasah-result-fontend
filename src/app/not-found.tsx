5641278f876836c27bf7104a52a6ffd70b67d668
Abdur Rahaman <rashedhasan967@gmail.com>
2025-03-25 01:38:55 +0600

    not found to home page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,6 @@
 
 
+import ButtonLink from '@/components/links/ButtonLink';
 import { Metadata } from 'next';
 import Link from 'next/link';
 import * as React from 'react';
@@ -9,15 +10,32 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center font-kalpurush">
-      <h2 className="text-2xl font-bold mb-4">পেজটি পাওয়া যায়নি!</h2>
-      <p className="text-gray-600 mb-4">দুঃখিত, আপনি যে পেজটি খুঁজছেন তা পাওয়া যায়নি।</p>
-      <Link
-        href="/search"
-        className="bg-gray-600 text-white px-4 py-2 rounded-md hover:bg-gray-700 transition-colors"
-      >
-        হোম পেজে ফিরে যান
-      </Link>
-    </div>
+    <section style={{ fontFamily: 'kalpurush', width: '100%', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} className='bg-white flex justify-center items-center font-kalpurush'>
+      <div style={{
+        display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
+        gap: '-2px'
+      }} className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
+        <img
+          src='/images/logo.jpg'
+          alt='জাতীয় দ্বীনি মাদরাসা শিক্ষাবোর্ড বাংলাদেশ লোগো'
+          className='mx-auto mb-2 h-20 rounded-full'
+          width={80}
+          height={80}
+        />
+        <h3 style={{ marginBottom: '-10px', }} className='mb-8 text-xl font-bold'>
+          {' '}
+          জাতীয় দ্বীনি মাদরাসা শিক্ষাবোর্ড বাংলাদেশ
+        </h3>
+        <p style={{ marginBottom: '5px', }} className='text-sm font mt-4'>রেসাল্ট দেখতে নীচের বাটনে ক্লিক করুন</p>
+        <ButtonLink
+          size='sm'
+          variant='outline'
+          className='text-blue-500'
+          href='/search'
+        >
+          রেসাল্ট পেজ
+        </ButtonLink>
+      </div>
+    </section>
   );
 }
